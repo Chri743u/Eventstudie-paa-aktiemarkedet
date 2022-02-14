52d3403f8a337ead80fca58104863f26cf5ffa59
LarsFG <ljk153@alumni.ku.dk>
2022-02-14 11:27:23 +0100

    opg1

--- opg1.R
+++ opg1.R
@@ -2,4 +2,12 @@ rm(list = ls()) # Clear environment
 cat("\014")  # Clear console # ctrl+L 
 
 #opgave 1
-#-46:12
+#Hvor meget er der tjent ved køb d. 5. januar og salg d. 30 marts (-46:12)
+# Ri+1
+
+
+
+for(i in -46:12)
+{
+  
+}
