8ccab07bff7a9b3d51667881ff718c052d45c095
LarsFG <ljk153@alumni.ku.dk>
2022-02-14 13:17:56 +0100

    ...

--- opg1.R
+++ opg1.R
@@ -3,11 +3,15 @@ cat("\014")  # Clear console # ctrl+L
 
 #opgave 1
 #Hvor meget er der tjent ved køb d. 5. januar og salg d. 30 marts (-46:12)
-# Ri+1
+#(\pi_{n=-46}^{12} (Ri_n+1)-1)*10.000.000
 
+Berk=(data.frame(BerkHathLubrizol))
+i=Berk$EventTime
 
 
 for(i in -46:12)
 {
+  afkast_i=subset(Berk,Ri==i)
+  
   
 }
